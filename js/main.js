008c9bf0bf8b99dad7594aa6fb2980ca9e7069f0
chrisbrody <chrisbrody@codifyacademy.com>
2022-03-18 12:01:00 -0400

    added JS for swiper Testimonials section

--- js/main.js
+++ js/main.js
@@ -20,3 +20,18 @@ faqs.forEach(faq => {
         }
     });
 });
+
+// swiper testimonials
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    breakpoints :{
+        600: {
+            slidesPerView: 2
+        }
+    }
+})
