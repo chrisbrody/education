169f294173d8e56e6790346e2bb37d2534ae3f95
chrisbrody <chrisbrody@codifyacademy.com>
2022-03-22 12:01:00 -0400

    added JS for mobile nav toggle

--- js/main.js
+++ js/main.js
@@ -34,4 +34,25 @@ var swiper = new Swiper(".mySwiper", {
             slidesPerView: 2
         }
     }
-})
+});
+
+// show / hide mobile nav
+const menu = document.querySelector(".nav__menu");
+const openBtn = document.querySelector("#open-menu-btn");
+const closeBtn = document.querySelector("#close-menu-btn");
+
+const openNav = () => {
+    menu.style.display = 'flex';
+    closeBtn.style.display = 'inline-block';
+    openBtn.style.display = 'none';
+}
+
+openBtn.addEventListener('click', openNav);
+
+const closeNav = () => {
+    menu.style.display = 'none';
+    closeBtn.style.display = 'none';
+    openBtn.style.display = 'inline-block';
+}
+
+closeBtn.addEventListener('click', closeNav);
