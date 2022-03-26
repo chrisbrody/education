080e7f2c6cdbdc87b18da022f7170aaa942a9ec5
chrisbrody <chrisbrody@codifyacademy.com>
2022-03-26 12:01:00 -0400

    added CSS for about page achievements section

--- js/main.js
+++ js/main.js
@@ -3,39 +3,6 @@ window.addEventListener('scroll', () => {
     document.querySelector('nav').classList.toggle('window-scroll', window.scrollY > 0);
 });
 
-// show/hide faq answers
-const faqs = document.querySelectorAll('.faq');
-let faqactive = false;
-
-faqs.forEach(faq => {
-    faq.addEventListener('click', () => {
-        faq.classList.toggle('open');
-
-        // change icon
-        const icon = faq.querySelector('.faq__icon i');
-        if(icon.className === 'uil uil-plus') {
-            icon.className = 'uil uil-minus';
-        } else {
-            icon.className = 'uil uil-plus';
-        }
-    });
-});
-
-// swiper testimonials
-var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 1,
-    spaceBetween: 30,
-    pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-    },
-    breakpoints :{
-        600: {
-            slidesPerView: 2
-        }
-    }
-});
-
 // show / hide mobile nav
 const menu = document.querySelector(".nav__menu");
 const openBtn = document.querySelector("#open-menu-btn");
